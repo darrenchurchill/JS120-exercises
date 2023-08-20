0b3d1f975f7236c66ef7928d1ad29817129ab2c8
Darren Churchill <darrenmchurchill@gmail.com>
2023-08-20 16:06:35 -0700

    Add OO Basics: Classes: Create the Class

---
+++ 01_oo_basics_classes/02_create_the_class.js
@@ -0,0 +1,13 @@
+/**
+ * Darren Churchill
+ *
+ * Launch School Exercises
+ * JS120 - Object Oriented JavaScript
+ * OO Basics: Classes
+ *
+ * Create the Class
+ *
+ * https://launchschool.com/exercises/2f68f88c
+ */
+
+class Cat {}
