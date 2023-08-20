b1daf7758499ddf95bc50b2f8a33e9c83e8d91fa
Darren Churchill <darrenmchurchill@gmail.com>
2023-08-20 16:13:27 -0700

    Add OO Basics: Classes: What are you?

---
+++ 01_oo_basics_classes/04_what_are_you.js
@@ -0,0 +1,19 @@
+/**
+ * Darren Churchill
+ *
+ * Launch School Exercises
+ * JS120 - Object Oriented JavaScript
+ * OO Basics: Classes
+ *
+ * What are you?
+ *
+ * https://launchschool.com/exercises/f838286d
+ */
+
+class Cat {
+  constructor() {
+    console.log("I'm a cat!");
+  }
+}
+
+let kitty = new Cat();
