bf658ffcdc676ddf9cead1d97b923b0ea7e8d607
Darren Churchill <darrenmchurchill@gmail.com>
2023-08-20 16:28:45 -0700

    Add OO Basics: Classes: Generic Greeting (part 1)

---
+++ 01_oo_basics_classes/09_generic_greeting_pt1.js
@@ -0,0 +1,19 @@
+/**
+ * Darren Churchill
+ *
+ * Launch School Exercises
+ * JS120 - Object Oriented JavaScript
+ * OO Basics: Classes
+ *
+ * Generic Greeting (part 1)
+ *
+ * https://launchschool.com/exercises/f925ee46
+ */
+
+class Cat {
+  static genericGreeting() {
+    console.log("Hello! I'm a cat!");
+  }
+}
+
+Cat.genericGreeting();
