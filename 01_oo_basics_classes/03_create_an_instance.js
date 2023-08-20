fe39cc83db81a6392c3e01c456e266b96f661f5a
Darren Churchill <darrenmchurchill@gmail.com>
2023-08-20 16:09:55 -0700

    Add OO Basics: Classes: Create an Instance

---
+++ 01_oo_basics_classes/03_create_an_instance.js
@@ -0,0 +1,15 @@
+/**
+ * Darren Churchill
+ *
+ * Launch School Exercises
+ * JS120 - Object Oriented JavaScript
+ * OO Basics: Classes
+ *
+ * Create an Instance
+ *
+ * https://launchschool.com/exercises/6ffc74c2
+ */
+
+class Cat {}
+
+let kitty = new Cat();
