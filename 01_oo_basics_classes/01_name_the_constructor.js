49bac2cbb75761ed770ace952b32337003fe0de8
Darren Churchill <darrenmchurchill@gmail.com>
2023-08-20 15:53:48 -0700

    Add OO Basics: Classes: Name the Constructor

---
+++ 01_oo_basics_classes/01_name_the_constructor.js
@@ -0,0 +1,18 @@
+/**
+ * Darren Churchill
+ *
+ * Launch School Exercises
+ * JS120 - Object Oriented JavaScript
+ * OO Basics: Classes
+ *
+ * Name the Constructor
+ *
+ * https://launchschool.com/exercises/6b22c666
+ */
+
+// All objects have a constructor property referencing their constructor
+// function. All functions have a name property indicating the function's name
+// as specified at creation.
+console.log("Hello".constructor.name);
+console.log([1, 2, 3].constructor.name);
+console.log({ name: "Srdjan" }.constructor.name);
