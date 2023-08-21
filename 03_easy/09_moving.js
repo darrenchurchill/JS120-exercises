c7fac0d5c7db709d8ba2d006d79e485d84b1d212
Darren Churchill <darrenmchurchill@gmail.com>
2023-08-21 11:33:48 -0700

    Add Easy: Moving template code

---
+++ 03_easy/09_moving.js
@@ -0,0 +1,53 @@
+/**
+ * Darren Churchill
+ *
+ * Launch School Exercises
+ * JS120 - Object Oriented JavaScript
+ * Easy
+ *
+ * Moving
+ *
+ * https://launchschool.com/exercises/6d2071cb
+ */
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  gait() {
+    return "strolls";
+  }
+}
+
+class Cat {
+  constructor(name) {
+    this.name = name;
+  }
+
+  gait() {
+    return "saunters";
+  }
+}
+
+class Cheetah {
+  constructor(name) {
+    this.name = name;
+  }
+
+  gait() {
+    return "runs";
+  }
+}
+
+let mike = new Person("Mike");
+console.log(mike.walk());
+// "Mike strolls forward"
+
+let kitty = new Cat("Kitty");
+console.log(kitty.walk());
+// "Kitty saunters forward"
+
+let flash = new Cheetah("Flash");
+console.log(flash.walk());
+// "Flash runs forward"
