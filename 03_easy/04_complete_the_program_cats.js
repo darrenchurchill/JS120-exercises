829816d3c7622ca1f7277740af2e265cb7f54b73
Darren Churchill <darrenmchurchill@gmail.com>
2023-08-20 18:38:23 -0700

    Add Easy: Complete the Program - Cats!

---
+++ 03_easy/04_complete_the_program_cats.js
@@ -0,0 +1,35 @@
+/**
+ * Darren Churchill
+ *
+ * Launch School Exercises
+ * JS120 - Object Oriented JavaScript
+ * Easy
+ *
+ * Complete the Program - Cats!
+ *
+ * https://launchschool.com/exercises/94c9e258
+ */
+
+class Pet {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Cat extends Pet {
+  constructor(name, age, color) {
+    super(name, age);
+    this.color = color;
+  }
+
+  info() {
+    return `My cat ${this.name} is ${this.age} years old and has ${this.color} fur.`;
+  }
+}
+
+let pudding = new Cat('Pudding', 7, 'black and white');
+let butterscotch = new Cat('Butterscotch', 10, 'tan and white');
+
+console.log(pudding.info());
+console.log(butterscotch.info());
