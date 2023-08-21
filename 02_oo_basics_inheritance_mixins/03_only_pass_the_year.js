4236c0ec5faca7fc78b234a6deb61e8f1731bc2a
Darren Churchill <darrenmchurchill@gmail.com>
2023-08-20 17:11:17 -0700

    Add OO Basics: Inheritance and Mixins: Only Pass the Year

---
+++ 02_oo_basics_inheritance_mixins/03_only_pass_the_year.js
@@ -0,0 +1,30 @@
+/**
+ * Darren Churchill
+ *
+ * Launch School Exercises
+ * JS120 - Object Oriented JavaScript
+ * OO Basics: Inheritance and Mixins
+ *
+ * Only Pass the Year
+ *
+ * https://launchschool.com/exercises/036df07f
+ */
+
+class Vehicle {
+  constructor(year) {
+    this.year = year;
+  }
+}
+
+class Truck extends Vehicle {
+  constructor(year, bedType) {
+    super(year);
+    this.bedType = bedType;
+  }
+}
+
+class Car extends Vehicle {}
+
+let truck1 = new Truck(2003, 'Short');
+console.log(truck1.year);
+console.log(truck1.bedType);
