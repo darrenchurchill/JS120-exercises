47403fbc4080da45eee2fb1a86f05e2cf26c6701
Darren Churchill <darrenmchurchill@gmail.com>
2023-08-21 17:16:30 -0700

    Add Objects: School solution code

--- 04_objects/05_school.js
+++ 04_objects/05_school.js
@@ -37,6 +37,33 @@ function createStudent(name, year) {
       console.log(Object.values(this.courses));
     },
 
+    addGrade(courseCode, grade) {
+      this.courses[courseCode].grade = grade;
+    },
+
+    hasGrade(courseName) {
+      return !Number.isNaN(Number(this.getGrade(courseName)));
+    },
+
+    getGrade(courseName) {
+      let course = Object.values(this.courses).filter(
+        (course) => course.name === courseName
+      )[0];
+      if (course) {
+        if (course.grade !== undefined) return course.grade;
+        return "In progress";
+      }
+      return `${this.name} is not taking ${courseName}`;
+    },
+
+    getGrades() {
+      return Object.values(this.courses)
+        .map((course) => {
+          return `${course.name}: ${course.grade ? course.grade : "In progress"}`;
+        })
+        .join("\n");
+    },
+
     /**
      *
      * @param {number} courseCode
@@ -67,23 +94,88 @@ function createStudent(name, year) {
 }
 
 let school = {
+  /** @type {Object.<string, {name: string, year: string, courses: {name: string, code: number, grade: number}}} */
+  students: {},
+
+  /**
+   *
+   * @param {string} name
+   * @param {string} year
+   */
   addStudent(name, year) {
+    let validYears = ["1st", "2nd", "3rd", "4th", "5th"];
 
+    if (!validYears.includes(year)) {
+      console.log("Invalid Year");
+      return;
+    }
+    let student = createStudent(name, year);
+    this.students[name] = student;
+    return student;
   },
 
+  /**
+   *
+   * @param {string} name
+   * @param {{name: string, code: number}} course
+   */
   enrollStudent(name, course) {
-
+    this.students[name].addCourse(course);
   },
 
-  addGrade(name, course) {
-
+  addGrade(name, courseCode, grade) {
+    this.students[name].addGrade(courseCode, grade);
   },
 
-  getReportCard(name) {
-
+  getReportCard(student) {
+    return this.students[student.name].getGrades();
   },
 
-  courseReport(course) {
+  courseReport(courseName) {
+    let grades = {};
 
+    for (let student of Object.values(this.students)) {
+      if (student.hasGrade(courseName)) {
+        grades[student.name] = student.getGrade(courseName);
+      }
+    }
+
+    if (Object.keys(grades).length > 0) {
+      console.log(`=${courseName} Grades=`);
+      Object.entries(grades)
+        .forEach(([name, grade]) => console.log(`${name}: ${grade}`));
+      console.log("---");
+      console.log(
+        "Course Average:",
+        Object.values(grades).reduce((tot, grade) => tot + grade) /
+          Object.values(grades).length
+      );
+    }
   },
 };
+
+let foo = school.addStudent("foo", "3rd");
+school.enrollStudent("foo", {name: "Math", code: 101});
+school.enrollStudent("foo", {name: "Advanced Math", code: 102});
+school.enrollStudent("foo", {name: "Physics", code: 202});
+
+let bar = school.addStudent("bar", "1st");
+school.enrollStudent("bar", {name: "Math", code: 101});
+
+let qux = school.addStudent("qux", "2nd");
+school.enrollStudent("qux", {name: "Math", code: 101});
+school.enrollStudent("qux", {name: "Advanced Math", code: 102});
+
+school.addGrade("foo", 101, 95);
+school.addGrade("foo", 102, 90);
+
+school.addGrade("bar", 101, 91);
+
+school.addGrade("qux", 101, 93);
+school.addGrade("qux", 102, 90);
+
+console.log(school.getReportCard(foo));
+
+school.courseReport("Math");
+school.courseReport("Advanced Math");
+school.courseReport("Physics");
